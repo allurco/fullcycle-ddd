e1880c2b165c35f7845766687e60638a76a31eef
Rogers Sampaio <r@allur.co>
2023-09-14 23:13:21 -0300

    Remove old main code

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,3 @@ import Customer from "./domain/entity/customer";
 import Order from "./domain/entity/order";
 import OrderItem from "./domain/entity/order_item";
 
-let customer = new Customer("123", "John");
-customer.address = new Address("123", "Seattle", "WA", "12345");
-customer.activate();
-
-const item1 = new OrderItem("1", 1, "Apple", 1);
-const item2 = new OrderItem("2", 2, "Orange", 2);
-
-const order = new Order("1", "123", [item1, item2]);
